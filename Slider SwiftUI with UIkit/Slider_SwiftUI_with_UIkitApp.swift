4e686800bf6ff340cef15fdbbd566c1d49edfe60
seregious <seregious@gmail.com>
2022-04-26 16:09:16 +0300

    Initial Commit

---
+++ Slider SwiftUI with UIkit/Slider_SwiftUI_with_UIkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slider_SwiftUI_with_UIkitApp.swift
+//  Slider SwiftUI with UIkit
+//
+//  Created by Сергей Иванчихин on 26.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Slider_SwiftUI_with_UIkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
